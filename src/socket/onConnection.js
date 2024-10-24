345020a28a653a24053c2ae76411f7639dd7ed40
purebver <wkddj1541@gmail.com>
2024-10-24 15:03:23 +0900

    Feat: "소켓 이벤트 기초"

--- src/socket/onConnection.js
+++ src/socket/onConnection.js
@@ -1,5 +1,13 @@
+import { onData } from './events/onData.js';
+import { onEnd } from './events/onEnd.js';
+import { onError } from './events/onError.js';
+
 export const onConnection = (socket) => {
-  console.log(`클라이언트 연결 주소 + 임시포트:${socket.remoteAddress}:${socket.remotePort}`);
+  console.log(`클라이언트 연결 주소:${socket.remoteAddress}:${socket.remotePort}`);
 
   socket.buffer = Buffer.alloc(0);
+
+  socket.on('data', onData(socket));
+  socket.on('end', onEnd(socket));
+  socket.on('error', onError(socket));
 };
